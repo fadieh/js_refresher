31bb104ad160b4205d8cd3ad1478052ec08be91a
Fadie Hannona <fadiehannona@gmail.com>
2015-02-24 16:03:35 +0000

    first commit

---
+++ src/jsRefresher.js
@@ -0,0 +1,123 @@
+// Question 1
+
+function selectElementsStartingWithE(array) {
+  
+};
+
+// Question 2
+
+function selectElementsStartingWithVowel(array) {
+
+};
+
+// Question 3
+
+function removeNullsFromArray(array) {
+
+};
+
+// Question 4
+
+function removeNullsAndFalseFromArray(array) {
+
+};
+
+// Question 5
+
+function reverseEveryElementInArray(array) {
+
+};
+
+// Question 6
+
+function dropFirstThreeElements(array) {
+
+};
+
+// Question 7
+
+function addElementToBeginningOfArray(array, element) {
+
+};
+
+// Question 8
+
+function sortArrayByLastLetterOfEachWord(array) {
+
+};
+
+// Question 9
+
+function returnFirstHalfOfString(string) {
+
+};
+
+// Question 10
+
+function makeNumberNegative(number) {
+
+};
+
+// Question 11
+
+function separateArrayIntoEvenAndOddNumbers(array) {
+
+};
+
+// Question 12
+
+function numberOfElementsThatArePalindromes(array) {
+
+};
+
+// Question 13
+
+function shortestWordInArray(array) {
+
+};
+
+// Question 14
+
+function longestWordInArray(array) {
+
+};
+
+// Question 15
+
+function arrayTotal(array) {
+
+};
+
+// Question 16
+
+function doubleArray(array) {
+
+};
+
+// Question 17
+
+function averageOfArray(array) {
+
+};
+
+// Question 18
+
+function removeElementsGreaterThanFive(array) {
+
+};
+
+function isLessThanFive(element) {
+
+};
+
+// Question 19
+
+function convertArrayToObject(array) {
+
+};
+
+// Question 20
+
+function getLettersInArrayOfWords(array) {
+
+};
